80b8c7f1bc5d6b57b3f892c96dd4625e4778a42f
dougalcaleb <dougalcaleb@gmail.com>
2021-03-17 10:14:56 -0600

    formatting

--- src/app/services/orchestrator.service.ts
+++ src/app/services/orchestrator.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { StoreService as Store } from "./store.service";
+import {Injectable} from "@angular/core";
+import {StoreService as Store} from "./store.service";
 
 @Injectable({
 	providedIn: "root",
